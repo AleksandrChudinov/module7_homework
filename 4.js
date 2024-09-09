9a017b1c6572c4cd962c0f51b67360a207cad332
Aleksandr <123251018+AleksandrChudinov@users.noreply.github.com>
2024-09-09 19:14:03 +0500

    Update 4.js

--- 4.js
+++ 4.js
@@ -1,7 +1,7 @@
 function setFullName(fullName) {
   this.fullName = fullName;
 }
-let person = {};
+const person = {};
 const setPersonFullName = setFullName.bind(person);
 setPersonFullName("John Smith");
 console.log(person);
